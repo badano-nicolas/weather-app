3e73c9fa37465a4c1791eb8e0d7fc22d100a02ed
badano-nicolas <badano.nicolas@gmail.com>
2022-05-08 21:08:47 -0300

    Added axios to connect to the api, added headlessui and heroicons for dropwdown, simple styling

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from "react";
 import Header from "./components/Header";
-import DefaultPlaces from "./components/DefaultPlaces";
+import CitySelector from "./components/CitySelector";
 import TodayWeather from "./components/TodayWeather";
 import WeeeklyWeather from "./components/WeeeklyWeather";
 
 function App() {
   return (
     <div>
-      <Header/>
-      <DefaultPlaces/>
-      <TodayWeather/>
-      <WeeeklyWeather/>
+      <Header />
+      <CitySelector />
+      <TodayWeather />
+      <WeeeklyWeather />
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Header = () => {
     return (
-        <div className='max-w-[800px] w-full mx-auto text-center flex flex-col justify-center'>
+        <div className='max-w-[800px] w-full mx-auto text-center flex flex-col'>
             <h1 className='w-full text-3xl font-bold text-[#00df9a]'>El tiempo Luján</h1>
         </div>
     )

da2f96f91b602f8334ebeec149789ff0b0594ff9
badano-nicolas <badano.nicolas@gmail.com>
2022-05-08 21:33:11 -0300

    Removed some code, changed idea

--- src/components/CitySelector.jsx
+++ src/components/CitySelector.jsx
@@ -1,70 +1,42 @@
-import React, { Fragment } from 'react';
-import { Menu, Transition } from '@headlessui/react';
-import { ChevronDownIcon } from '@heroicons/react/solid';
+import React from 'react';
 import cities from '../lib/cities.json'
-
-
-function classNames(...classes) {
-  return classes.filter(Boolean).join(' ');
-}
 
 const CitySelector = () => {
   return (
     <div className='max-w-[800px] w-full h-screen mx-auto text-center flex flex-col mt-5'>
 
       <p className='text-[#00df9a] font-bold p-2'>
-        EL TIEMPO EN OTRAS
+        VER EL TIEMPO EN OTRAS CIUDADES
       </p>
 
 
 
       <div className='w-full'>
-        <Menu as='div' className='relative inline-block text-left w-56'>
-          <div>
-            <Menu.Button className='inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500'>
-              Ciudades
-              <ChevronDownIcon
-                className='-mr-1 ml-2 h-5 w-5'
-                aria-hidden='true'
-              />
-            </Menu.Button>
-          </div>
-
-          <Transition
-            as={Fragment}
-            enter='transition ease-out duration-100'
-            enterFrom='transform opacity-0 scale-95'
-            enterTo='transform opacity-100 scale-100'
-            leave='transition ease-in duration-75'
-            leaveFrom='transform opacity-100 scale-100'
-            leaveTo='transform opacity-0 scale-95'
-          >
-            <Menu.Items className='origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 divide-y divide-gray-100 focus:outline-none'>
-
+        <div class="flex justify-center">
+          <div class="mb-3 xl:w-96">
+            <select class="form-select appearance-none
+              block
+              w-full
+              px-3
+              py-1.5
+              text-base
+              font-normal
+              text-gray-700
+              bg-white bg-clip-padding bg-no-repeat
+              border border-solid border-gray-300
+              rounded
+              transition
+              ease-in-out
+              m-0
+              focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" aria-label="City selector">
+              <option selected>Ciudad actual</option>
               {cities.map((city) => {
                 return (
-                  <div className='py-1'>
-                    <Menu.Item>
-                      {({ active }) => (
-                        <a
-                          
-                          className={classNames(
-                            active
-                              ? 'bg-gray-100 text-gray-900'
-                              : 'text-gray-700',
-                            'block px-4 py-2 text-sm'
-                          )}
-                        >
-                          {city.name}
-                        </a>
-                      )}
-                    </Menu.Item>
-                  </div>
-                )
-              })}
-            </Menu.Items>
-          </Transition>
-        </Menu>
+                  <option value={city.id} key={city.id}>{city.name}</option>
+                )})}
+            </select>
+          </div>
+        </div>
       </div>
     </div>
   )
